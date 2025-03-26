8959612c02e040c577c4496296bae0a203edb8da
Tshewang Dhendup <tshewang@Tshewangs-MacBook-Pro.local>
2025-03-26 12:13:13 +0530

    Python basic code addition

--- test.py
+++ test.py
@@ -1,3 +1,14 @@
 print("This is Test File")
 print ("File created in RUB brunch")
 print ("Added after addition of file in local")
+
+stu = "Hello"
+#print("stu: {stu} data type: {type: stu}")
+
+def greeting():
+    message = "Hi, How are you?"
+    print(message)
+    print("There are", len(message), "Words in the Greeting message")
+
+greeting()
+
